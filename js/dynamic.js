cb4418493ff02fe77371ee78566a1791fbab190b
Alexandr <justdealman@gmail.com>
2015-03-31 08:31:00 +0700

    index

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,11 +1,4 @@
-﻿function productDescription() {
-	$('.production ul li > div h5').each(function() {
-		$(this).css({
-			'margin-top': -$(this).height()/2+4+'px'
-		});
-	});
-}
-function factsLiH5() {
+﻿function factsLiH5() {
 	$('.facts .proof ul li h5').each(function() {
 		$(this).css({
 			'margin-top': ($(this).parent().height()-$(this).height())/2+'px'
@@ -24,27 +17,81 @@ function slider() {
 		pause: 2500,
 		slidesLoaded: function() {
 			var first = $('.slider .slides_control > div:first-child');
-			first.find('.using').html(first.find('.hidden').html());
-			first.find('.using .chart').drawDoughnutChart([
-				{ value: 47, color:'#f8be00' },
-				{ value: 63, color:'#057c46' },
-				{ value: 73, color:'#02483e' },
-				{ value: 180, color:'#9bb61b' }
-			]);
-			first.find('.using .grow').addClass('complete');
-			first.find('.using .tip').css({
-				'left': first.find('.using .tip').attr('data-left')+'px',
-				'top': first.find('.using .tip').attr('data-top')+'px'
-			});
-			first.find('.using .tip').find('h6').css({
-				'width': first.find('.using .tip h6').attr('data-width')
-			});
-			first.find('.tip h3').css({
-				'margin-top': -first.find('.tip h3').height()/2+'px'
-			});
-			setTimeout(function() {
-				first.find('.using .tip').addClass('complete');
-			}, 1250);
+			if ( first.attr('class') == 'slide1' ) {
+				first.find('.using').html(first.find('.hidden').html());
+				first.find('.using .chart').drawDoughnutChart([
+					{ value: 47, color:'#f8be00' },
+					{ value: 63, color:'#057c46' },
+					{ value: 73, color:'#02483e' },
+					{ value: 180, color:'#9bb61b' }
+				]);
+				first.find('.using .grow').addClass('complete');
+				first.find('.using .tip').css({
+					'left': first.find('.using .tip').attr('data-left')+'px',
+					'top': first.find('.using .tip').attr('data-top')+'px'
+				});
+				first.find('.using .tip').find('h6').css({
+					'width': first.find('.using .tip h6').attr('data-width')
+				});
+				first.find('.tip h3').css({
+					'margin-top': -first.find('.tip h3').height()/2+'px'
+				});
+				setTimeout(function() {
+					first.find('.using .tip').addClass('complete');
+				}, 1250);
+			}
+			if ( first.attr('class') == 'slide2' ) {
+				first.find('.using').html(first.find('.hidden').html());
+				first.find('.using .graph img').css({
+					'bottom': -first.find('.using .graph img').attr('height')+'px'
+				});
+				setTimeout(function() {
+					first.find('.using .grow').addClass('complete');
+				}, 100);
+				setTimeout(function() {
+					first.find('.using .graph img').stop(true,true).animate({
+						'bottom': '0'
+					}, 2000, 'easeOutExpo');
+				}, 850);
+				first.find('.tip h3').css({
+					'margin-top': -first.find('.tip h3').height()/2+'px'
+				});
+				setTimeout(function() {
+					first.find('.using .tip').addClass('complete');
+				}, 1500);
+			}
+			if ( first.attr('class') == 'slide3' ) {
+				first.find('.using').html(first.find('.hidden').html());
+				setTimeout(function() {
+					first.find('.using .grow').addClass('complete');
+				}, 100);
+				setTimeout(function() {
+					first.find('.using .diagram img.col1').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 1000);
+				setTimeout(function() {
+					first.find('.using .diagram img.col2').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 1500);
+				setTimeout(function() {
+					first.find('.using .diagram img.col3').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 2000);
+				setTimeout(function() {
+					first.find('.using .diagram img.col4').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 2500);
+				first.find('.tip h3').css({
+					'margin-top': -first.find('.tip h3').height()/2+'px'
+				});
+				setTimeout(function() {
+					first.find('.using .tip').addClass('complete');
+				}, 3000);
+			}
 		},
 		animationStart: function() {
 			var prev = $('.slider .slides_control > div:nth-child('+eval($('.slider .pagination li.current').index()+1)+')');
@@ -55,31 +102,85 @@ function slider() {
 		},
 		animationComplete: function() {
 			var next = $('.slider .slides_control > div:nth-child('+eval($('.slider .pagination li.current').index()+1)+')');
-			next.find('.using').html(next.find('.hidden').html());
-			next.find('.using .chart').drawDoughnutChart([
-				{ value: 47, color:'#f8be00' },
-				{ value: 63, color:'#057c46' },
-				{ value: 73, color:'#02483e' },
-				{ value: 180, color:'#9bb61b' }
-			]);
-			next.find('.using .grow').addClass('complete');
-			next.find('.using .tip').css({
-				'left': next.find('.using .tip').attr('data-left')+'px',
-				'top': next.find('.using .tip').attr('data-top')+'px'
-			});
-			next.find('.using .tip h6').css({
-				'width': next.find('.using .tip').find('h6').attr('data-width')
-			});
-			next.find('.using .tip h3').css({
-				'margin-top': -next.find('.using .tip').find('h3').height()/2+'px'
-			});
-			setTimeout(function() {
-				next.find('.using .tip').addClass('complete');
-			}, 1250);
+			if ( next.attr('class') == 'slide1' ) {
+				next.find('.using').html(next.find('.hidden').html());
+				next.find('.using .chart').drawDoughnutChart([
+					{ value: 47, color:'#f8be00' },
+					{ value: 63, color:'#057c46' },
+					{ value: 73, color:'#02483e' },
+					{ value: 180, color:'#9bb61b' }
+				]);
+				next.find('.using .grow').addClass('complete');
+				next.find('.using .tip').css({
+					'left': next.find('.using .tip').attr('data-left')+'px',
+					'top': next.find('.using .tip').attr('data-top')+'px'
+				});
+				next.find('.using .tip h6').css({
+					'width': next.find('.using .tip').find('h6').attr('data-width')
+				});
+				next.find('.using .tip h3').css({
+					'margin-top': -next.find('.using .tip').find('h3').height()/2+'px'
+				});
+				setTimeout(function() {
+					next.find('.using .tip').addClass('complete');
+				}, 1250);
+			}
+			if ( next.attr('class') == 'slide2' ) {
+				next.find('.using').html(next.find('.hidden').html());
+				next.find('.using .graph img').css({
+					'bottom': -next.find('.using .graph img').attr('height')+'px'
+				});
+				setTimeout(function() {
+					next.find('.using .grow').addClass('complete');
+				}, 100);
+				setTimeout(function() {
+					next.find('.using .graph img').stop(true,true).animate({
+						'bottom': '0'
+					}, 2000, 'easeOutExpo');
+				}, 850);
+				next.find('.tip h3').css({
+					'margin-top': -next.find('.tip h3').height()/2+'px'
+				});
+				setTimeout(function() {
+					next.find('.using .tip').addClass('complete');
+				}, 1500);
+			}
+			if ( next.attr('class') == 'slide3' ) {
+				next.find('.using').html(next.find('.hidden').html());
+				setTimeout(function() {
+					next.find('.using .grow').addClass('complete');
+				}, 100);
+				setTimeout(function() {
+					next.find('.using .diagram img.col1').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 1000);
+				setTimeout(function() {
+					next.find('.using .diagram img.col2').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 1500);
+				setTimeout(function() {
+					next.find('.using .diagram img.col3').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 2000);
+				setTimeout(function() {
+					next.find('.using .diagram img.col4').stop(true,true).animate({
+						'bottom': '0'
+					}, 1000);
+				}, 2500);
+				next.find('.tip h3').css({
+					'margin-top': -next.find('.tip h3').height()/2+'px'
+				});
+				setTimeout(function() {
+					next.find('.using .tip').addClass('complete');
+				}, 3000);
+			}
 		}
 	});
 }
-function geoList() {
+/*function geoList() {
 	$('.geography > div > ul li').append('<span class="before"><em><em></em></em></span>');
 	$('.geography > div > ul li').append('<span class="after"><em><em></em></em></span>');
 	var col1 = $('.geography > div > ul li.col1');
@@ -160,7 +261,7 @@ function geoList() {
 			'top': $(this).attr('data-top')+'px'
 		});
 	});
-}
+}*/
 function map() {
 	$('.contactus #map').css({
 		'height': $('.wrapper').height()-$('.header').height()+'px'
@@ -182,7 +283,6 @@ $(document).ready(function() {
 		$('.production').append('<span></span>');
 		var ps = Math.ceil(($(window).width()/2-438)/393)*393;
 		$('.production > span').width(ps);
-		productDescription();
 	}
 	if ( $('.about').length > 0 ) {
 		$('.about .region > div p').hover(
@@ -203,9 +303,6 @@ $(document).ready(function() {
 				}, 400);
 			}
 		);
-	}
-	if ( $('.geography').length > 0 ) {
-		geoList();
 	}
 	if ( $('.facts').length > 0 ) {
 		var fph = 0;
@@ -272,6 +369,30 @@ $(document).ready(function() {
 	if ( $('.contactus #map').length > 0 ) {
 		map();
 	}
+	$('.modal').append('<span class="close"></span>');
+	var bh = 0;
+	$('.header button').bind('click', function() {
+		$('.modal').css({
+			'margin-top': -($(window).height()-($('.modal').outerHeight()+60))/2+'px'
+		}).addClass('slideInDown').removeClass('slideOutDown').stop(true,true).fadeIn(500);
+		$('.fade').stop(true,true).fadeIn(500);
+		bh = $(document).scrollTop();
+		$('body').css({'position': 'fixed', 'top': -bh+'px', 'overflow-y': 'scroll'});
+		return false;
+	});
+	$('.modal .close, .fade').bind('click', function() {
+		$('.modal').addClass('slideOutDown').removeClass('slideInDown')
+		$('.modal, .fade').stop(true,true).fadeOut(500);
+		$('body').css({'position': 'static', 'top': '0', 'overflow-y': 'auto'});
+		$(document).scrollTop(bh);
+		return false;
+	});
+	$('.geography .nav li a').bind('click', function() {
+		$(this).parents('.geography').find('.map img').stop(true,true).fadeOut(500);
+		$(this).parents('.geography').find('.map img[class="'+$(this).attr('class')+'"]').stop(true,true).fadeIn(500);
+		$(this).parent().addClass('active').siblings().removeClass('active');
+		return false;
+	}).filter(':first').click();
 });
 $(window).resize(function() {
 	if ( $('.contactus #map').length > 0 ) {
@@ -279,9 +400,6 @@ $(window).resize(function() {
 	}
 });
 $(window).load(function() {
-	if ( $('.production').length > 0 ) {
-		productDescription();
-	}
 	if ( $('.facts').length > 0 ) {
 		factsLiH5();
 	}
